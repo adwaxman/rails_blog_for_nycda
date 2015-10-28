7214c69670ba3268dc898d93420e4cc252790fd5
Adam Waxman <adwaxman@gmail.com>
2015-10-28 17:52:37 -0400

    Add create posts feature, keeping all db interaction in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
@@ -7,7 +8,25 @@ class PostsController < ApplicationController
   end
 
   def create
+    puts "******************* PARAMS ********************"
+    puts params
+    body = params[:post][:body]
+    id = current_user.id
+    @post = Post.new(user_id: id, body: body)
+    # @post = Post.new(post_params)
+    if @post.save
+      flash[:notice] = "Your post was created successfully."
+      redirect_to user_path current_user
+    else
+      flash[:alert] = "There was a problem. Please try again"
+      redirect_to user_path current_user
+    end
+  end
 
+  private
+
+  def post_params
+    params.require(:post).permit(:body, :user_id)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,7 @@ Rails.application.routes.draw do
 
   get '/search' => 'users#search'
 
-
-  get '/posts' => 'posts#index'
+  resources :posts
 
 
 
